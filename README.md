42b4c9ac8486191fdbab137981a4c56d53055ca3
Simon Shi <simonsmh@gmail.com>
2019-11-19 15:46:51 +0800

    closes #25

--- README.md
+++ README.md
@@ -38,7 +38,7 @@ Please follow the instuction below from @axeldna:
 
 * You should use latest Magisk Manager to install this module. If you meet any problem under installation from Magisk Manager, please try to install it from recovery.
 * Recent fixes:
-New installation logic by finding target file with GNU find.
+Ignore commented lines.
 
 ## Credit & Support
 
--- install.sh
+++ install.sh
@@ -142,9 +142,9 @@ on_install() {
     [[ -f /sbin/.magisk/mirror$SELECTPATH ]] && cp -af /sbin/.magisk/mirror$SELECTPATH $MODPATH$SELECTPATH || cp -af $SELECTPATH $MODPATH$SELECTPATH
     ui_print "- Starting modifiy"
     sed -i 's/gChannelBondingMode24GHz=0/gChannelBondingMode24GHz=1/g;s/gChannelBondingMode5GHz=0/gChannelBondingMode5GHz=1/g;s/#gChannelBondingMode24GHz=1/gChannelBondingMode24GHz=1/g;s/#gChannelBondingMode5GHz=1/gChannelBondingMode5GHz=1/g;s/gForce1x1Exception=1/gForce1x1Exception=0/g;s/#gForce1x1Exception=0/gForce1x1Exception=0/g' $MODPATH$SELECTPATH
-    [[ -z $(grep gChannelBondingMode24GHz $MODPATH$SELECTPATH) ]] && sed -i 's/^END$/gChannelBondingMode24GHz=1\nEND/g' $MODPATH$SELECTPATH
-    [[ -z $(grep gChannelBondingMode5GHz $MODPATH$SELECTPATH) ]] && sed -i 's/^END$/gChannelBondingMode5GHz=1\nEND/g' $MODPATH$SELECTPATH
-    [[ -z $(grep gForce1x1Exception $MODPATH$SELECTPATH) ]] && sed -i 's/^END$/gForce1x1Exception=0\nEND/g' $MODPATH$SELECTPATH
+    [[ -z $(grep -v "^#" $MODPATH$SELECTPATH | grep gChannelBondingMode24GHz) ]] && sed -i 's/^END$/gChannelBondingMode24GHz=1\nEND/g' $MODPATH$SELECTPATH
+    [[ -z $(grep -v "^#" $MODPATH$SELECTPATH | grep gChannelBondingMode5GHz) ]] && sed -i 's/^END$/gChannelBondingMode5GHz=1\nEND/g' $MODPATH$SELECTPATH
+    [[ -z $(grep -v "^#" $MODPATH$SELECTPATH | grep gForce1x1Exception) ]] && sed -i 's/^END$/gForce1x1Exception=0\nEND/g' $MODPATH$SELECTPATH
   }
   done
   [[ -z $SELECTPATH ]] && abort "- Installation FAILED. Your device didn't support WCNSS_qcom_cfg.ini." || { mkdir -p $MODPATH/system; mv -f $MODPATH/vendor $MODPATH/system/vendor;}
